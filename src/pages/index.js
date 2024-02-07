620fbb6c4272be3ef309054d431ee49e98df6592
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2024-02-07 10:05:12 +0300

    feat: create banner and add carousel unfinished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon Clone | Betul</title>
       </Head>
 
       <Header />
-      {/* Banner */}
-      {/* Product */}
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        {/* Product */}
+      </main>
     </div>
   );
 }
